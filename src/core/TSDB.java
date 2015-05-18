94db69e5374417bdef4b2c67316bc194277f37bb
Ovidiu Maja <ovidiu.maja@tora.com>
2015-05-18 14:38:50 +0300

    Modified value for tag lenght to 4 bytes and max number of tags to 20.

--- src/core/TSDB.java
+++ src/core/TSDB.java
@@ -69,7 +69,7 @@ public final class TSDB {
   private static final String TAG_NAME_QUAL = "tagk";
   private static final short TAG_NAME_WIDTH = 3;
   private static final String TAG_VALUE_QUAL = "tagv";
-  private static final short TAG_VALUE_WIDTH = 3;
+  private static final short TAG_VALUE_WIDTH = 4;
 
   /** Client for the HBase cluster to use.  */
   final HBaseClient client;
